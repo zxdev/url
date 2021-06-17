86a07c95e11e91b9181e7a1a81dc9557de8cf7e1
zxdev <24780673+zxdev@users.noreply.github.com>
2021-06-17 10:40:06 -0700

    add deschemer

--- scheme.go
+++ scheme.go
@@ -1,15 +1,17 @@
+// MIT License
+//
 // Copyright (c) 2020 zxdev
-
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,34 +22,15 @@
 
 package url
 
-import (
-	"crypto/sha256"
-	"fmt"
-)
+import "strings"
 
-type fingerprint struct{ Host, FP string }
+// RemoveScheme helper removes all text before ://
+func RemoveScheme(url string) string {
 
-// Fingerprint is a sha256 fingerprint generator utility for url.URL
-//	fp[0] is u.Host
-//	fp[1] is eTLD+1 and only present when u.Host != eTLD+1
-// sha256: 1cbec737f863e4922cee63cc2ebbfaafcd1cff8b790d8cfd2e6a5d550b648afa
-func Fingerprint(u *URL) (fp []fingerprint) {
-
-	h := sha256.New()
-	h.Write([]byte(u.Host))
-	fp = append(fp, fingerprint{u.Host, fmt.Sprintf("%x", h.Sum(nil))})
-
-	host, err := u.EffectiveTLDPlusOne()
-	if err == nil {
-		h.Reset()
-		h.Write([]byte(host))
-		if len(fp[0].Host) != len(host) {
-			fp = append(fp, fingerprint{host, fmt.Sprintf("%x", h.Sum(nil))})
-		}
+	n := strings.Index(url, "://")
+	if n > -1 {
+		return url[n+3:]
 	}
 
-	h.Write([]byte(u.Host))
-	fp = append(fp, fingerprint{u.Host, fmt.Sprintf("%x", h.Sum(nil))})
-
-	return fp
+	return url
 }
